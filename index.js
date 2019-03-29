0cad4da08b5446d72f20764a3bc227859d55041a
Pablo Ruiz Pardo <prp1987@gmail.com>
2019-03-29 15:28:41 +0000

    nuevo método restore

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 app.use(express.urlencoded())
-app.use(express.bodyParser());
+app.use(express.json());
 
 
 
